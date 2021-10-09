9d1f2ef776408027e4acd11e3bfea86eab7c1d73
pavelpetrov <petrovengineer@gmail.com>
2021-10-09 23:42:35 +0300

    Finish project config

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    parser: '@babel/eslint-parser',
+    env: {
+        browser: true,
+        node: true,
+        es6: true
+    },
+    extends: ['eslint:recommended', 'google'],
+    rules: {
+        'semi': 'off',
+        'comma-dangle': 'off',
+        'indent': ['error', 4]
+    }
+}
